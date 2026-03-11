3f8d29d34541d4cc2590c8bdbca8be202ef8f637
Om Patel <om.patel@iitgn.ac.in>
2026-03-12 02:34:35 +0530

    Update 4-Bit Adder using Full Adder Module Testbench.v

--- 4-Bit Adder using Full Adder Module Testbench.v
+++ 4-Bit Adder using Full Adder Module Testbench.v
@@ -10,5 +10,26 @@ module Four_bit_Adder_tb ();
     .num_out(num_out),
     .final_carry(final_carry)
     );
+
+    initial begin 
+        $monitor("Time=%0t | number_1=%b | number_2 = %b | Output sum = %b | Output carry = %b", $time,number_1,number_2,num_out,final_carry);
+        number_1 = 4'b0101;
+        number_2 = 4'b0011;
+        #10;
+        number_1 = 4'b0100;
+        number_2 = 4'b0011;
+        #10;
+        number_1 = 4'b0001;
+        number_2 = 4'b0010;
+        #10;
+        number_1 = 4'b0100;
+        number_2 = 4'b0100;
+        #10;
+        number_1 = 4'b1000;
+        number_2 = 4'b1000;
+        #10;
+        number_1 = 4'b1001;
+        number_2 = 4'b1001;
+    end
     
 endmodule
