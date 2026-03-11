940c33137e974ff68f8e70ef6bf79ccf3f6cfcf7
Om Patel <om.patel@iitgn.ac.in>
2026-03-11 22:35:07 +0530

    Create 4-Bit Adder using Full Adder Module Testbench.v

---
+++ 4-Bit Adder using Full Adder Module Testbench.v
@@ -0,0 +1,14 @@
+module Four_bit_Adder_tb ();
+    reg [3:0] number_1;
+    reg [3:0] number_2;
+    wire [3:0] num_out;
+    wire final_carry;
+    
+    Four_bit_Adder Instance_1 (
+    .number_1(number_1),
+    .number_2(numeber_2),
+    .num_out(num_out),
+    .final_carry(final_carry)
+    );
+    
+endmodule
